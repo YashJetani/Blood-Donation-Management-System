b31a3723491b001c0e7e2a8c3213d422728e4984
YashJetani <yashjetani123@gmail.com>
2025-03-14 22:07:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,71 +11,10 @@
 			</div>
 
 
-			<h1 class="text-center">Search The Donors</h1>
-			<hr class="white-bar text-center">
+<!-- 			<h1 class="text-center">Search The Donors</h1>
+			<hr class="white-bar text-center"> -->
 
-			<form action="search.php" method="get" class="form-inline text-center" style="padding: 40px 0px 0px 5px;">
-				<div class="form-group text-center justify-content-center">
-
-					<select style="width: 220px; height: 45px;" name="city" id="city" class="form-control demo-default"
-						required>
-						<option value="" style="text-align:center">-- Select City --</option>
-						<optgroup title="Azad Jammu and Kashmir (Azad Kashmir)"
-							label="&raquo; GUJARAT"></optgroup>
-							<option value="AHMADABAD">AHMADABAD</option>
-						<option value="AMRELI">AMRELI</option>
-						<option value="ANAND">ANAND</option>
-						<option value="ARAVALLI">ARAVALLI</option>
-						<option value="BANASKANTHA">BANASKANTHA</option>
-						<option value="BHARUCH">BHARUCH</option>
-						<option value="BHAVNAGAR">BHAVNAGAR</option>
-						<option value="BOTAD">BOTAD</option>
-						<option value="CHHOTA UDEPUR">CHHOTA UDEPUR</option>
-						<option value="DAHOD">DAHOD</option>
-						<option value="DANGS">DANGS</option>
-						<option value="DEVBHUMI DWARKA">DEVBHUMI DWARKA</option>
-						<option value="GANDHINAGAR">GANDHINAGAR</option>
-						<option value="GIR SOMNATH">GIR SOMNATH</option>
-						<option value="JAMNAGAR">JAMNAGAR</option>
-						<option value="JUNAGADH">JUNAGADH</option>
-						<option value="KACHCHH">KACHCHH</option>
-						<option value="KHEDA">KHEDA</option>
-						<option value="MAHESANA">MAHESANA</option>
-						<option value="MAHISAGAR">MAHISAGAR</option>
-						<option value="MORBI">MORBI</option>
-						<option value="NARMADA">NARMADA</option>
-						<option value="NAVSARI">NAVSARI</option>
-						<option value="PANCHMAHALS">PANCHMAHALS</option>
-						<option value="PATAN">PATAN</option>
-						<option value="PORBANDAR">PORBANDAR</option>
-						<option value="RAJKOT">RAJKOT</option>
-						<option value="SABARKANTHA">SABARKANTHA</option>
-						<option value="SURAT">SURAT</option>
-						<option value="SURENDRANAGAR">SURENDRANAGAR</option>
-						<option value="TAPI">TAPI</option>
-						<option value="VADODARA">VADODARA</option>
-						<option value="VALSAD">VALSAD</option>
-					</select>
-				</div>
-				<div class="form-group center-aligned">
-					<select name="blood_group" id="blood_group" style="padding: 0 20px; width: 220px; height: 45px;"
-						class="form-control demo-default text-center margin10px">
-						<option value="" text-align="center">-- blood group --</option>
-						<option value="A+">A+</option>
-						<option value="A-">A-</option>
-						<option value="B+">B+</option>
-						<option value="B-">B-</option>
-						<option value="AB+">AB+</option>
-						<option value="AB-">AB-</option>
-						<option value="O+">O+</option>
-						<option value="O-">O-</option>
-
-					</select>
-				</div>
-				<div class="form-group center-aligned">
-					<button type="submit" class="btn btn-lg btn-danger">Search</button>
-				</div>
-			</form>
+			
 		</div>
 	</div>
 </div>
