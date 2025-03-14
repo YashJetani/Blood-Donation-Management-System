34642f6c7c15c7c747b3ee7d7578e707d32377ea
YashJetani <yashjetani123@gmail.com>
2025-03-14 22:47:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -24,21 +24,21 @@
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wallpaperflare.com%2Fsearch%3Fwallpaper%3D1920x1080&psig=AOvVaw1fakM8CL7FHF8-WPJ8w9Y7&ust=1742058441099000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPiU78aHiowDFQAAAAAdAAAAABAQ" class="d-block w-100" alt="...">
+      <img src="img/header.png" class="d-block w-100" alt="...">
       <div class="carousel-caption d-md-block">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fcool-1920x1080-wallpapers--955115033460220908%2F&psig=AOvVaw1fakM8CL7FHF8-WPJ8w9Y7&ust=1742058441099000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPiU78aHiowDFQAAAAAdAAAAABAE" class="d-block w-100" alt="...">
+      <img src="img/img2.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-md-block">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pixelstalk.net%2Fhd-wallpaper-1920x1080%2F&psig=AOvVaw1fakM8CL7FHF8-WPJ8w9Y7&ust=1742058441099000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPiU78aHiowDFQAAAAAdAAAAABAI" class="d-block w-100" alt="...">
+      <img src="img/img3.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
