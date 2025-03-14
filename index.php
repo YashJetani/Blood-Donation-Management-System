cff88764b8729c6a16190748fa107328e1924fe7
YashJetani <yashjetani123@gmail.com>
2025-03-14 22:32:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,41 +15,31 @@
 ">
 				<h1 class="text-center">Donate the blood, save the life</h1>
 				<p class="text-center">Donate the blood to help the others.</p>
-			</div>
-
-
-<!-- 			<h1 class="text-center">Search The Donors</h1>
-			<hr class="white-bar text-center"> -->
-
+</div>
 			
 		</div>
 	</div>
 </div> -->
 <!-- header ends -->
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/header.png" class="d-block w-100" alt="header image">
-      <div class="carousel-caption d-none d-md-block">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-md-block">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="img/header.png" class="d-block w-100" alt="header image">
-      <div class="carousel-caption d-none d-md-block">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-md-block">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="img/header.png" class="d-block w-100" alt="header image">
-      <div class="carousel-caption d-none d-md-block">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
       </div>
@@ -64,6 +54,7 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+
 
 <!-- donate section -->
 <div class="container-fluid red-background">
