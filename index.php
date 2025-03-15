07b23e94e8d4f4b1349bf1c8b0e0deb0978f1019
YashJetani <yashjetani123@gmail.com>
2025-03-15 10:47:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 </head>
 <body>
 
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+<div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="3000">
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="img/header.png" class="d-block w-100" alt="Slide 1">
@@ -48,6 +48,14 @@
       </div>
     </div>
   </div>
+	 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
 
 
