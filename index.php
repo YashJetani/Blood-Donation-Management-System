149dd4f18dc55acf4ac90122c8125d2e36ff4ce9
YashJetani <yashjetani123@gmail.com>
2025-03-15 17:38:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -42,8 +42,8 @@
 
 <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="3000" data-bs-pause="false" p-0 m-0>
   <div class="carousel-inner">
+	   <div class="overlay">
     <div class="carousel-item active">
-	     <div class="overlay"></div> 
       <img src="img/img10.jpg" class="d-block w-100 mb-0" alt="Slide 1" style="opacity: 0.6;">
       <div class="carousel-caption d-md-block">
         <h5>Donate the blood, save the life</h5>
@@ -51,7 +51,6 @@
       </div>
     </div>
     <div class="carousel-item">
-	     <div class="overlay"></div> 
       <img src="img/img11.jpg" class="d-block w-100 mb-0" alt="Slide 2" style="opacity: 0.6;">
       <div class="carousel-caption d-md-block">
         <h5>Donate the blood, save the life</h5>
@@ -59,13 +58,13 @@
       </div>
     </div>
     <div class="carousel-item">
-	     <div class="overlay"></div> 
       <img src="img/img8.jpg" class="d-block w-100 mb-0" alt="Slide 3" style="opacity: 0.6;">
       <div class="carousel-caption d-md-block">
         <h5>Donate the blood, save the life</h5>
         <p>Donate the blood to help the others.</p>
       </div>
     </div>
+  </div>
   </div>
 	 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -75,6 +74,7 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
+	
 </div>
 
 
