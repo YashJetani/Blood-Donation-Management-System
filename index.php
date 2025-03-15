eccf7a47b101e9c4c7b1666034fe3ea7741e554c
YashJetani <yashjetani123@gmail.com>
2025-03-15 17:59:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
     left: 0;
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.7); /* Adjust the opacity for darker effect */
+    background: rgba(0, 0, 0, 0.6); /* Adjust the opacity for darker effect */
     z-index: 1;
 }
 
