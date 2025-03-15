49f6cce33dfab84247cb1e36aee63ab227d4f622
YashJetani <yashjetani123@gmail.com>
2025-03-15 10:09:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 	include ('include/header.php');
 ?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<head>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</head>
 
 <body>
 <!-- <div class="container-fluid header-img">
